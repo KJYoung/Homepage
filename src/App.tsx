b3eff22ddb1f33f9315dc49b7dcacae5ab3ca968
KJYoung <jykim157@snu.ac.kr>
2023-02-26 20:50:12 +0900

    [Setting] gh-pages, typescript

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>
+          홈페이지.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
