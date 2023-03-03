7b15cc683dab6d38b0cf09aa9692d2c94fcaac7c
KJYoung <jykim157@snu.ac.kr>
2023-03-03 17:14:20 +0900

    [Feat] Logo(Tour Eiffel)

--- src/containers/Private.tsx
+++ src/containers/Private.tsx
@@ -3,7 +3,6 @@ const Private = () => {
     return <div>
         <div>
             <span>개인</span>
-            <img src={process.env.PUBLIC_URL + "/logo192.png"} alt="Logo"></img>
         </div>
     </div>
 };
